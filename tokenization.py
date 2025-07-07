8ae244069c1fb587411b9eebef712b5e36fd24dc
RishabhWDB <rishabharya.k@gmail.com>
2025-07-07 16:35:11 +0530

    third

--- tokenization.py
+++ tokenization.py
@@ -2,12 +2,17 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 
-sentences = ['I love my dog', 'I love my cat', 'Peter ate a really fat cat']
+sentences = ['I love my dog', 'I love my cat', 'Peter ate a very fat cat']
 
-tokenizer = Tokenizer(num_words=100)
+tokenizer = Tokenizer(num_words=100, oov_token = "<OOV>")
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences(sentences)
+
+test_data = ['I really love my dog', 'my dog loves my manatee']
+
+test_seq = tokenizer.texts_to_sequences(test_data)
+print(test_seq)
 print(word_index)
-print(sequences)
+#print(sequences)
