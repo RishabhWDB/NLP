5856a9332849fdbd718d429a2ef76985c8b02b44
RishabhWDB <rishabharya.k@gmail.com>
2025-07-07 16:15:16 +0530

    second

--- tokenization.py
+++ tokenization.py
@@ -2,9 +2,12 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 
-sentences = ['I love my dog', 'I love my cat']
+sentences = ['I love my dog', 'I love my cat', 'Peter ate a really fat cat']
 
 tokenizer = Tokenizer(num_words=100)
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
+
+sequences = tokenizer.texts_to_sequences(sentences)
 print(word_index)
+print(sequences)
