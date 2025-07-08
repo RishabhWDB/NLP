b750474e4981c61aafd5f966d5416a0dd0bbe86d
RishabhWDB <rishabharya.k@gmail.com>
2025-07-08 10:22:07 +0530

    fifth

--- tokenization.py
+++ tokenization.py
@@ -2,9 +2,10 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.text import Tokenizer # type: ignore
+from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
 
-sentences = ['I love my dog', 'I love my cat', 'Peter ate a very fat cat']
+sentences = ['I love my dog', 'I love my cat', 'Peter ate a very fat cat', 'Don\'t you think my dog is amazing']
 
 tokenizer = Tokenizer(num_words=100, oov_token = "<OOV>")
 tokenizer.fit_on_texts(sentences)
@@ -12,9 +13,11 @@ word_index = tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences(sentences)
 
-test_data = ['I really love my dog', 'my dog loves my manatee']
+#test_data = ['I really love my dog', 'my dog loves my manatee']
 
-test_seq = tokenizer.texts_to_sequences(test_data)
-print(test_seq)
+#test_seq = tokenizer.texts_to_sequences(test_data)
+#print(test_seq)
+padded = pad_sequences(sequences)
 print(word_index)
-#print(sequences)
+print(sequences)
+print(padded)
