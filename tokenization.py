fc380fed04af1e16f8f9b65eef881909703f7cc2
Arya <rishabharya.k@gmail.com>
2025-07-08 23:07:55 +0530

    post padding

--- tokenization.py
+++ tokenization.py
@@ -5,7 +5,10 @@ from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer # type: ignore
 from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
 
-sentences = ['I love my dog', 'I love my cat', 'Peter ate a very fat cat', 'Don\'t you think my dog is amazing']
+sentences = ['I love my dog', 
+             'I love my cat',
+             'Peter ate a very fat cat',
+             'Don\'t you think my dog is amazing']
 
 tokenizer = Tokenizer(num_words=100, oov_token = "<OOV>")
 tokenizer.fit_on_texts(sentences)
@@ -17,7 +20,7 @@ sequences = tokenizer.texts_to_sequences(sentences)
 
 #test_seq = tokenizer.texts_to_sequences(test_data)
 #print(test_seq)
-padded = pad_sequences(sequences)
+padded = pad_sequences(sequences, padding = 'post')
 print(word_index)
 print(sequences)
 print(padded)
